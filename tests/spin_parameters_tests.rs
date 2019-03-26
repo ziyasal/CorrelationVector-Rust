b2935c106c8fa8e3789c5daa16647957e27cdb14
Ziλa Sarikaya <zisarika@microsoft.com>
2019-03-26 13:55:21 +0100

    small arrangements

--- tests/spin_parameters_tests.rs
+++ tests/spin_parameters_tests.rs
@@ -2,7 +2,7 @@ use correlation_vector;
 use correlation_vector::enums::SpinEntropy;
 
 #[test]
-fn it_adds_two() {
+fn it_sets_entropy() {
     let  mut sp = correlation_vector::SpinParameters::default();
 
     sp.set_entropy(Box::new(SpinEntropy::Four));
